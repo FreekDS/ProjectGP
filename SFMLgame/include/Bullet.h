b30d23c43347fc6b3b696d36b12aed9f6e3e6197
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2018-12-02 17:28:09 +0100

    Created SFML Bullet

---
+++ SFMLgame/include/Bullet.h
@@ -0,0 +1,14 @@
+#ifndef ROADFIGHTER_SFML_BULLET_H
+#define ROADFIGHTER_SFML_BULLET_H
+
+#include "GLL/Bullet.h"
+
+namespace RoadFighterSFML {
+
+    class Bullet : public RoadFighter::Bullet {
+
+    };
+
+}
+
+#endif //ROADFIGHTER_SFML_BULLET_H
