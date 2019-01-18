c5f730b1a536e11cb232052eaf2125aea4b49c63
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2019-01-18 12:42:13 +0100

    Fixed travis build error

--- SFMLgame/src/Game.cpp
+++ SFMLgame/src/Game.cpp
@@ -75,7 +75,7 @@ namespace RoadFighterSFML {
 
                     // check if the game has finished and change game state if so
                     if (m_world->gameFinished()){
-                        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+                        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter))
                             m_state = GameState::SCOREBOARD;
                     }
 
